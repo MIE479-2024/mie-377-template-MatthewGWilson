f1704f980b6a379152afb3eef34aaa6fd10a81eb
MatthewGWilson <matthew.g.wilson@gmail.com>
2024-11-14 14:51:17 -0500

    Updated to run optimization with gpt provided weights

--- services/project_function.py
+++ services/project_function.py
@@ -1,6 +1,11 @@
 from services.strategies import *
 
-def project_function(periodReturns, periodFactRet, x0, short):
-    Strategy = OLS_MVO()
-    x = Strategy.execute_strategy(periodReturns, periodFactRet, short)
+def project_function(periodReturns, periodFactRet, x0, short, strategy, weights):
+    if strategy == "OLS_MVO_ChatGPT":
+        Strategy = OLS_MVO_ChatGPT()
+        x = Strategy.execute_strategy(periodReturns, periodFactRet, short)
+    elif strategy == "ChatGPT_Weights":
+        Strategy = ChatGPT_Weights()
+        x = Strategy.execute_strategy(weights)
+    
     return x
--- services/strategies.py
+++ services/strategies.py
@@ -2,42 +2,7 @@ import numpy as np
 from services.estimators import *
 from services.optimization import *
 
-def equal_weight(periodReturns):
-    """
-    computes the equal weight vector as the portfolio
-    :param periodReturns:
-    :return:x
-    """
-    T, n = periodReturns.shape
-    x = (1 / n) * np.ones([n])
-    return x
-
-
-class HistoricalMeanVarianceOptimization:
-    # uses historical returns to estimate the covariance matrix and expected return
-
-    def __init__(self, NumObs=36):
-        self.NumObs = NumObs  # number of observations to use
-
-    def execute_strategy(self, periodReturns, factorReturns=None):
-        """
-        executes the portfolio allocation strategy based on the parameters in the __init__
-
-        :param periodReturns:
-        :param factorReturns:
-        :return: x
-        """
-        factorReturns = None  # we are not using the factor returns
-        returns = periodReturns.iloc[(-1) * self.NumObs:, :]
-        print(len(returns))
-        mu = np.expand_dims(returns.mean(axis=0).values, axis=1)
-        Q = returns.cov().values
-        x = MVO(mu, Q)
-
-        return x
-
-
-class OLS_MVO:
+class OLS_MVO_ChatGPT:
     # uses historical returns to estimate the covariance matrix and expected return
 
     def __init__(self, NumObs=36):
@@ -57,4 +22,20 @@ class OLS_MVO:
         factRet = factorReturns.iloc[(-1) * self.NumObs:, :]
         mu, Q = OLS(returns, factRet)
         x = MVO(mu, Q, short)
+        return x
+    
+class ChatGPT_Weights:
+    # uses chatgpt provided weights
+
+    def __init__(self, NumObs=36):
+        self.NumObs = NumObs  # number of observations to use
+
+    def execute_strategy(self, weights):
+        """
+        executes the portfolio allocation strategy based on the parameters in the __init__
+
+        :param weights:
+        :return:x
+        """
+        x = weights
         return x
