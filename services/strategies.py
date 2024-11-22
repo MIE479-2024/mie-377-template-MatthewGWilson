0fa9e57120245caacc864a6bd51898274e5fb321
MatthewGWilson <matthew.g.wilson@gmail.com>
2024-11-21 22:22:11 -0500

    Added Robust MVO and CVaR optimization

--- services/strategies.py
+++ services/strategies.py
@@ -40,3 +40,56 @@ class ChatGPT_Weights:
         """
         x = weights
         return x
+    
+
+class OLS_Robust_MVO_ChatGPT:
+    # uses historical returns to estimate the covariance matrix and expected return
+
+    def __init__(self, NumObs=36):
+        self.NumObs = NumObs  # number of observations to use
+
+    def execute_strategy(self, periodReturns, factorReturns, short):
+        """
+        executes the portfolio allocation strategy based on the parameters in the __init__
+
+        :param factorReturns:
+        :param periodReturns:
+        :param short:
+        :return:x
+        """
+        T, n = periodReturns.shape
+        # get the last T observations
+        returns = periodReturns.iloc[(-1) * self.NumObs:, :]
+        factRet = factorReturns.iloc[(-1) * self.NumObs:, :]
+        mu, Q = OLS(returns, factRet)
+        lambda_value = 20
+        alpha_value = 0.9
+        N = len(periodReturns)
+        x = Robust_MVO(mu, Q, lambda_value, alpha_value, N, short)
+        return x
+
+
+class OLS_CVAR_ChatGPT:
+    # uses historical returns to estimate the covariance matrix and expected return
+
+    def __init__(self, NumObs=36):
+        self.NumObs = NumObs  # number of observations to use
+
+    def execute_strategy(self, periodReturns, factorReturns, short):
+        """
+        executes the portfolio allocation strategy based on the parameters in the __init__
+
+        :param factorReturns:
+        :param periodReturns:
+        :param short:
+        :return:x
+        """
+        T, n = periodReturns.shape
+        # get the last T observations
+        returns = periodReturns.iloc[(-1) * self.NumObs:, :]
+        factRet = factorReturns.iloc[(-1) * self.NumObs:, :]
+        mu, Q = OLS(returns, factRet)
+        alpha_value = 0.9
+        N = len(periodReturns)
+        x = CVAR(mu, Q, alpha_value, returns, factRet, short)
+        return x
